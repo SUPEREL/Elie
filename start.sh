fb0d8e3f0f30233deabd60c2386657a47892d9a7
SUPEREL <108293740+SUPEREL@users.noreply.github.com>
2022-07-09 19:19:24 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TechnoMindz/IMDB_AutoDeleteRobot.git /IMDB_AutoDeleteRobot
+  git clone https://github.com/SUPEREL/Elie.git /Elie
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /IMDB_AutoDeleteRobot
+  git clone $UPSTREAM_REPO /Elie
 fi
-cd /IMDB_AutoDeleteRobot
+cd /Elie
 pip3 install -U -r requirements.txt
 echo "Starting Techno Mindz..."
 python3 bot.py
