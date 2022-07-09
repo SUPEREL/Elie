62a3937ce49dbdc6dcec7aa72769989d6df7f21e
SUPEREL <108293740+SUPEREL@users.noreply.github.com>
2022-07-09 15:03:51 +0100

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -41,7 +41,7 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("Vérifiez votre propre requête 😤", show_alert=True)
+        return await query.answer("Efectuez votre propre requête 😤", show_alert=True)
     try:
         offset = int(offset)
     except:
@@ -120,14 +120,14 @@ async def next_page(bot, query):
 async def advantage_spoll_choker(bot, query):
     _, user, movie_ = query.data.split('#')
     if int(user) != 0 and query.from_user.id != int(user):
-        return await query.answer("Vérifiez votre propre requête Chef 😤", show_alert=True)
+        return await query.answer("Efectuez votre propre requête Chef 😤", show_alert=True)
     if movie_ == "close_spellcheck":
         return await query.message.delete()
     movies = SPELL_CHECK.get(query.message.reply_to_message.message_id)
     if not movies:
         return await query.answer("Vous cliquez sur un ancien bouton qui a expiré.", show_alert=True)
     movie = movies[(int(movie_))]
-    await query.answer('Checking for Movie in database...')
+    await query.answer('Recherche du film dans la BD...')
     k = await manual_filters(bot, query.message, text=movie)
     if k == False:
         files, offset, total_results = await get_search_results(movie, offset=0, filter=True)
@@ -135,7 +135,7 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit('Ce film est introuvable dans ma base de données')
+            k = await query.message.edit('Ce film est introuvable dans ma BD')
             await asyncio.sleep(10)
             await k.delete()
 
@@ -803,7 +803,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"♨️Voici le résultat de votre requête <STRONG>{search}</STRONG>\n\n➥ ℝ𝕖𝕛𝕠𝕚𝕟𝕕𝕣𝕖 ➼ @VisionsTv"
+        cap = f"♨️Voici le résultat de votre requête sur 👉 <STRONG>{search}</STRONG>\n\n➥ ℝ𝕖𝕛𝕠𝕚𝕟𝕕𝕣𝕖 ➼ @VisionsTv pour plud de fun😍"
     if imdb and imdb.get('poster'):
         try:
             autodelete = await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
